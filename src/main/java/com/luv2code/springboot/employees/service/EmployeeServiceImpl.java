4fd625f33e46e7d841c3a5d916fff8f584528ea7
Mahmoud Gaber <71694567+MahmooudGaaber@users.noreply.github.com>
2025-06-04 05:24:57 +0300

    Employee : Switch app to JPARepository from DAO

--- src/main/java/com/luv2code/springboot/employees/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/employees/service/EmployeeServiceImpl.java
@@ -1,6 +1,6 @@
 package com.luv2code.springboot.employees.service;
 
-import com.luv2code.springboot.employees.dao.EmployeeDAO;
+import com.luv2code.springboot.employees.dao.EmployeeRepository;
 import com.luv2code.springboot.employees.entity.Employee;
 import com.luv2code.springboot.employees.request.EmployeeRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,45 +8,53 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO){
-        employeeDAO = theEmployeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository){
+        employeeRepository = theEmployeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(long theId) {
-        return employeeDAO.findById(theId);
+        Optional<Employee> result = employeeRepository.findById(theId);
+        Employee theEmployee = null ;
+        if(result.isPresent()){
+            theEmployee = result.get();
+        } else {
+            throw new RuntimeException("Did not find Employee Id -"+theId);
+        }
+        return theEmployee ;
     }
 
     @Override
     @Transactional
     public Employee save(EmployeeRequest employeeRequest) {
         Employee theEmployee = convertToEmployee(0,employeeRequest);
-        return employeeDAO.save(theEmployee);
+        return employeeRepository.save(theEmployee);
     }
 
     @Override
     @Transactional
     public Employee update(long id, EmployeeRequest theEmployeeRequest) {
         Employee theEmployee = convertToEmployee(id,theEmployeeRequest);
-        return employeeDAO.save(theEmployee);
+        return employeeRepository.save(theEmployee);
     }
 
     @Override
     @Transactional
     public void delete(long theId) {
-        employeeDAO.delete(theId);
+        employeeRepository.deleteById(theId);
     }
 
     @Override
