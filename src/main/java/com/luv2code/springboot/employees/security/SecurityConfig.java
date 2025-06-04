863eabff5a793b61bef5e44b75b0c571b8d645ef
Mahmoud Gaber <71694567+MahmooudGaaber@users.noreply.github.com>
2025-06-04 08:33:51 +0300

    Employee : Security Setup Users With Roles

--- src/main/java/com/luv2code/springboot/employees/security/SecurityConfig.java
+++ src/main/java/com/luv2code/springboot/employees/security/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.luv2code.springboot.employees.security;
 
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -31,6 +36,25 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity)throws Exception {
+
+        httpSecurity.authorizeHttpRequests(
+                configurer -> configurer
+                        .requestMatchers("/docs/**","/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.GET , "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET , "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST , "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT , "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE , "/api/employees/**").hasRole("ADMIN")
+        );
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        httpSecurity.csrf(csrf -> csrf.disable());
+        return  httpSecurity.build();
     }
 
 
