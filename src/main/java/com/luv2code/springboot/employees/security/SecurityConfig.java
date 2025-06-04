01debc277bdbb5df3daebed0803980172f9d9c34
Mahmoud Gaber <71694567+MahmooudGaaber@users.noreply.github.com>
2025-06-04 08:54:22 +0300

    Employee : Disable browser basic security

--- src/main/java/com/luv2code/springboot/employees/security/SecurityConfig.java
+++ src/main/java/com/luv2code/springboot/employees/security/SecurityConfig.java
@@ -4,11 +4,13 @@ import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -50,11 +52,26 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PUT , "/api/employees").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.DELETE , "/api/employees/**").hasRole("ADMIN")
         );
-
+        httpSecurity.httpBasic(httpBasicCustomizer -> httpBasicCustomizer.disable());
         httpSecurity.httpBasic(Customizer.withDefaults());
-
         httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.exceptionHandling(
+                exceptionHandlingError -> exceptionHandlingError
+                        .authenticationEntryPoint(authenticationEntryPoint())
+        );
         return  httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint(){
+        return ((request, response, authException) -> {
+           // send 401 unauthorized without triggering a basic auth
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            // remove the WWW-Authenticate header to prevent browser
+            response.setHeader("WWW-Authenticate","");
+            response.getWriter().write("{\"error\"UNAUTHORIZED Access \"}");
+        });
     }
 
 
